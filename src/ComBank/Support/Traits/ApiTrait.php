f8c2159a6f638dbc41d7736fd51ba418a3c4bbd7
Jowi <157366219+JowiMesas@users.noreply.github.com>
2024-11-19 18:39:32 +0100

    fraud

--- src/ComBank/Support/Traits/ApiTrait.php
+++ src/ComBank/Support/Traits/ApiTrait.php
@@ -1,5 +1,7 @@
 <?php namespace ComBank\Support\Traits;
 
+use ComBank\Transactions\Contracts\BankTransactionInterface;
+
 trait ApiTrait {
     //Convert Euros to Dollars 
     function convertBalance(float $euros): float {
@@ -21,7 +23,7 @@ trait ApiTrait {
     function validateEmail(string $email) : bool {
             $ch = curl_init();
             $api = "https://api.usercheck.com/email/$email?key=PaHQqILPLJDHrxXAf7Kans589FwvdjF2";
-            curl_setopt($ch, CURLOPT_URL, $api);
+            curl_setopt($ch, CURLOPT_URL, value: $api);
             curl_setopt_array($ch, array(
                 CURLOPT_RETURNTRANSFER => true,
                 CURLOPT_SSL_VERIFYPEER => false,
@@ -39,6 +41,28 @@ trait ApiTrait {
                 return false;
             }
 
+
+    }
+    function detectFraud(BankTransactionInterface $transaction): bool {
+        $ch = curl_init();
+        $api = "https://673789ef4eb22e24fca57b15.mockapi.io/fraudsystem";
+        curl_setopt($ch, CURLOPT_URL, value: $api);
+        curl_setopt_array($ch, array(
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_SSL_VERIFYPEER => false,
+        ));
+        $result = curl_exec(handle: $ch);
+        curl_close(handle: $ch);
+        $convertJson = json_decode ($result, true);
+        $giveAmount = $transaction->getAmount();
+        $fraud = false;
+        foreach ($convertJson as $key => $value) {
+            if($convertJson[$key]["movementType"]==$transaction->getTransactionInfo()) {
+                if($convertJson[$key]["amount"] >= $giveAmount && $convertJson[$key]["max_amount"] <= $giveAmount) {
+                    
+                }
+            }
+        }
 
     }
 }
